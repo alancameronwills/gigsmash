b768f2f5f9e85f41380eaf784206859d5704349f
Alan Cameron Wills <alan@cameronwills.org>
2025-05-02 07:27:08 +0100

    server for apache+passenger; favicon

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-/*
-* Very basic server for local testing or for running in bare nodejs environments.
-*
-* Otherwise, use Azure Functions or similar:
-* Requests to /api map to the directory /api and should be run on server; 
-* on Azure, set them up as js Functions.
-* 
-* Other requests should map to contents of /client and should be served up as is.
-* on Azure, put them in Blob store and map non-api calls to there.
-*
-* Default page is /client/index.html
-* Put client-side pages in /client
-* Put server-side scripts under /api, written in Javascript with a default export
-*
-*/
-
 
 const http = require('http');
 const util = require('util');
@@ -23,112 +7,35 @@ const process = require('process');
 
 const logverbose = false;
 
-const contentTypes = {
-	".css": "text/css",
-	".htm": "text/html", ".html": "text/html",
-	".ico" : "image/x-icon",
-	".gif": "image/gif",
-	".jpg": "image/jpeg", ".jpeg": "image/jpeg",
-	".js": "text/js",
-	".json": "application/json",
-	".mp3": "audio/mpeg", ".mp4": "video/mp4", ".mpeg": "video/mpeg",
-	".png": "image/png",
-	".pdf": "application/pdf",
-	".txt": "text/plain"
-};
 
 var server = null;
 
+
 const handlers = {
-	ping: async (request, response) => {
-		response.writeHead(200, { "Content-Type": "text/plain" });
-		response.end("pong " + new Date().toISOString());
+	ping: (request, response) => {
+		response.body = "pong " + new Date().toISOString();
 	},
 	stopserver: (request, response) => {
 		setTimeout (() => server.close(), 1000);
-		response.writeHead(200, { "Content-Type": "text/plain" });
-		response.end("Stopped " + new Date().toISOString());
+		response.body = "Stopped " + new Date().toISOString();
 	}
 };
 
 async function runFile(fPath, req, response) {
-	let context = { res:{body: "", status: 200, headers: {"Content-Type":"text/plain"}} , log:console.log};
+	let context = { res:response , log:console.log};
 	try {
 		let code = (await import(fPath)).default;
-		//log("Code " + util.inspect(code));
-		await code(context, req);
+		context.res.body = `Code5 ${util.inspect( code)} ${fPath}`;
+		await code (context, req);
 	} catch (e) {
-		context = { body: e.toString(), status: 500, contentType: "text/plain" };
+	    context.res.body = `runFile ${fPath} error ${e.toString()}`;
 	}
-	response.writeHead(context.res?.status ?? 500, context.res?.headers ?? {});
-	response.end(context.res?.body??"");
+	response.body = context.res.body;
+	response.status = context.res.status;
+	response.headers = context.res.headers;
+	return response;
 }
 
-
-
-(async () => {
-	let root = (await fs.realpath('.')).replace("/server", "");
-	// binds root
-	async function serve(request, response) {
-		try {
-			let req = parseReq(request);
-			verbose(req);
-			if (req.path.startsWith("/api/")) {
-				// run the file
-				let fPath = `${root}${req.path}`;
-				if (fPath.indexOf("..") < 0 && fsSync.existsSync(fPath)) {
-					if (fsSync.lstatSync(fPath).isDirectory()) {
-						fPath += "/index.js";
-					}
-					fPath = fPath.replace(/\\/g, "/").replace("C:", "file:///C:");
-					//log (`Loading ${fPath} ${fsSync.existsSync(fPath)}`);
-					await runFile(fPath, req, response);
-				} else {
-					response.writeHead(400);
-					response.end(`API not found: "${req.path}"`);
-				}
-			} else {
-				let fPath = `${root}/client${req.path}`;
-				fPath = fPath.replace(/\//g, "\\");//.replace("C:", "file:///C:");
-				verbose (`[${fPath}]` );
-				if (fPath.indexOf("..") < 0 && fsSync.existsSync(fPath) && !fsSync.lstatSync(fPath).isDirectory()) {
-					// return the file content
-					let reply = "";
-					let replyType = contentTypes[req.extension] ?? "text/plain";
-					let status = 200;
-					let file = `${root}/client${req.path}`;
-					try {
-						reply = await fs.readFile(file);
-					} catch (err) {
-						reply = err.toString();
-						replyType = "text/plain";
-						status = 400;
-					} finally {
-						response.writeHead(status, { "Content-Type": replyType });
-						response.end(reply);
-					}
-				} else {
-					let h = handlers[req.path.substring(1)];
-					if (h) {
-						h(request, response);
-					} else {
-						response.writeHead(400);
-						response.end(`Content not found "${req.path}"`);
-					}
-				}
-			}
-		} catch (err) {
-			log("Serve exception " + util.inspect(err));
-		}
-	}
-	server = http.createServer(serve);
-	const port = process.argv[2] || 80;
-	server.listen(port);
-	log(`Server running at http://localhost:${port}`);
-	server.on('close', () => {
-		log("Server closing");
-	})
-})()
 
 
 function parseReq(request, defaultPage = "/index.html") {
@@ -140,6 +47,7 @@ function parseReq(request, defaultPage = "/index.html") {
 	}
 	let host = headers.Host;
 	let path = url.replace(/[\?#].*/, "");
+	let path2 = path.replace(/^\/[^\/]*/,"");
 	if (path == "/") path = defaultPage;
 	let extension = path.match(/\.[^.]*$/)?.[0] ?? "";
 	let queryString = url.match(/\?(.*)/)?.[1] ?? "";  // url.replace(/.*\?/,"");
@@ -150,31 +58,49 @@ function parseReq(request, defaultPage = "/index.html") {
 		m[kv[0]] = kv.length > 1 ? kv[1] : true;
 		return m;
 	}, {});
-	return { path: path, extension: extension, queryString: queryString, query: query, host: host, url: url, method: method, headers: headers };
+	return { path: path, path2, extension: extension, queryString: queryString, query: query, host: host, url: url, method: method, headers: headers };
 }
+(async () => {
+var root = (await fs.realpath('.')).replace("/server", "");
+var count = 0;
 
-//******************
-// Logging
-//
-//  */
-function verbose(msg) {
-	if (logverbose) log(msg);
-}
-let previousMsg = "";
-let messageRepeatCount = 0;
-function log(msg, condition = true) {
-	if (!condition) return;
-	
-	if (previousMsg == msg) {
-		messageRepeatCount++;
-	} else {
-		if (messageRepeatCount > 0) {
-			console.log(` * ${messageRepeatCount}`);
-			messageRepeatCount = 0;
+server = http.createServer(async function(request, res) {
+    var message = "", version = "";
+    var response = {status:200,headers:{'Content-Type': 'text/plain'},body:"Nothing"};
+    try {
+		let req = parseReq(request);
+		let cmd = req.path2.substring(1);
+		//response.body = `Cmd [${cmd}]`;
+		let h = handlers[cmd];
+		
+		
+		if (h) {
+    		//response.body = `Immediate handler [${cmd}]`;
+			h(request, response);
+		} else {
+		    let fPath = `${root}/api${req.path2}`;
+		    //response.body = `fPath [${fPath}]`;
+			if (fPath.indexOf("..") < 0 && fsSync.existsSync(fPath)) {
+				if (fsSync.lstatSync(fPath).isDirectory()) {
+					fPath += "/index.js";
+			    }
+		        //response.body = `running [${fPath}]`;
+				
+			    await runFile(fPath, req, response);	
+		 
+		    } else {
+                message = `It works! ${count++} \n` ;
+                let q = JSON.stringify(req, null, "  ");
+                version = 'NodeJS ' + process.versions.node + '\n';
+                response.body = [message, q, root, version].join('\n');
+		    }
+			    
 		}
-		previousMsg = msg;
-		console.log(`${new Date().toISOString()}`, msg);
-	}
-}
-
-
+    } catch (e) {
+        response.body = "Error: " + e.toString();
+    }
+    res.writeHead(response.status, response.headers);
+    res.end(response.body);
+});
+server.listen();
+})();

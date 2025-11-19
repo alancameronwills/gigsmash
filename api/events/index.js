074522eade5a477be6908b8e28b5134f559572a7
Alan Cameron Wills <alan@cameronwills.org>
2025-11-19 08:02:01 +0000

    Collect api param; FilmOnSunday

--- api/events/index.js
+++ api/events/index.js
@@ -210,11 +210,8 @@ let handlers = [];
 
 (handlers["filmonsunday"] = async () => {
     let r = [];
-    try {
-        let sourceUrl = "https://filmonsunday.co.uk";
-        let source = await ftext(sourceUrl);
-        let eventSection = m(source, /<article[^>]*portfolio.*?>(.*)<\/article>/s);
-        let events = eventSection.match(/<li .*?>.*?<\/li>/gs);
+    let extract = body => {
+        let events = body.match(/<li .*?>.*?<\/li>/gs);
         events.forEach(event => {
             let ri = {};
             ri.image = m(event, /src="(https:.*?)"/s);
@@ -226,6 +223,37 @@ let handlers = [];
             ri.venue = "Templeton Village Hall";
             r.push(ri);
         });
+    }
+    try {
+        let sourceUrl = "https://filmonsunday.co.uk";
+        let source = await ftext(sourceUrl);
+        let eventSection = m(source, /<article[^>]*portfolio.*?>(.*)<\/article>/s);
+        extract(eventSection);
+
+        /* Load more */
+        //console.log(source);
+        let lmItems = m(source, /loadMoreItems:(.*?)\]\]\]/s);
+        let lm = {
+            data: [...lmItems.matchAll(/\[([0-9]+)/g)].map(x => `data%5B%5D=${x[1]}`).join('&'),
+            token: m(source, /loadMoreAjaxToken: *"(.*?)"/),
+            url: m(source, /loadMoreAjaxUrl: *"(.*)"/),
+            action: m(source, /loadMoreAjaxAction: *"(.*)"/),
+            gridId: m(source, /gridID: *([0-9]+)/)
+        }
+        //console.log(JSON.stringify(lm));
+        if (lm.action && lm.token) {
+            let payLoad = `action=${lm.action}&client_action=load_more_items&token=${lm.token}&${lm.data}&gridid=${lm.gridId}`;
+            //console.log(payLoad);
+            let more = await fetch(lm.url, {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" },
+                body: payLoad
+            }).then(r => r.json());
+            //console.log(more);
+            if (more.data) {
+                extract(more.data);
+            }
+        }
     } catch (e) { r.push({ e: e.toString() }); console.log(e); }
     return r;
 }).friendly = "Film On Sunday";
@@ -263,17 +291,17 @@ let handlers = [];
         try {
             //console.log("event: " + ri.title);
             let eventFile = await ftext(ri.url);
-            let content = m(eventFile,/<div[^>]*tribe-events-single-event-description.*?>(.*?)<\/div>/s);
+            let content = m(eventFile, /<div[^>]*tribe-events-single-event-description.*?>(.*?)<\/div>/s);
             //console.log(content);
             let img = m(content, /<img(.*?)>/s);
-            let src = m(img,/src=['"](.*?)['"]/s);
+            let src = m(img, /src=['"](.*?)['"]/s);
             //console.log(`${eventFile.length} ${content.length}  ${img}   === ${src}`);
             if (src) {
                 ri.image = src;
             }
         } catch (e) {
             //console.log(e);
-         }
+        }
     }));
     return r;
 }).friendly = "Narberth Jazz";
@@ -327,7 +355,9 @@ let handlers = [];
             ri.text = "";
             ri.price = m(event, /<span class="price">(.*?)<\/span>/s);
             ri.category = "live";
-            r.push(ri);
+            if (ri.dt) {
+                r.push(ri);
+            }
         })
 
     } catch (e) {
@@ -379,6 +409,12 @@ let handlers = [];
                         ri.title = columns?.[0 + isDoubleBill]?.replace(/<.*?>/sg, "")?.trim() || "AberJazz";
                         ri.venue = venue;
                         ri.url = m(columns?.[0 + isDoubleBill], /href=['"](.*?)['"]/s);
+                        if (ri.url.indexOf(https) != 0) {
+                            if (ri.url.indexOf("/") == 0)
+                                ri.url = "https://aberjazz.com" + ri.url;
+                            else
+                                ri.url = "https://aberjazz.com/en/html/" + ri.url;
+                        }
                         ri.text = "";
                         ri.dt = cdate;
                         ri.date = dateString;
@@ -706,26 +742,26 @@ let ticketsource = async (tsid) => {
     let r = [];
     eventsBlocks.forEach(block => {
         try {
-            let titleDiv = m(block,/<div [^>]*eventTitle.*?>(.*?)<\/div>/s);
+            let titleDiv = m(block, /<div [^>]*eventTitle.*?>(.*?)<\/div>/s);
             let title = m(titleDiv, /<span[^>]*"name".*?>(.*?)</s);
             let dateDiv = m(block, /(<div[^>]*startDate.*?<\/div>)/s);
             let event = {
                 title: title,
                 image: m(block, /src="(.*?)"/s),
                 url: `https://ticketsource.co.uk${m(titleDiv, /href="(.*?)"/s)}`,
-                date: dateDiv.replaceAll(/<.*?>\s*/gs,""),
+                date: dateDiv.replaceAll(/<.*?>\s*/gs, ""),
                 dt: new Date(m(dateDiv, /content="(.*?)"/s)).valueOf(),
                 venue: m(block, /<div[^>]*venueAddress.*?>(.*?)<\/span>/s).replaceAll(/<.*?>/gs, ""),
-                category: title.indexOf("NTLive")>=0 ? "broadcast" : "live"
+                category: title.indexOf("NTLive") >= 0 ? "broadcast" : "live"
             };
             if (event.title && event.dt && event.dt > now) {
                 r.push(event);
             }
-        } catch(e) {
-            r.push({e:e.toString()});
+        } catch (e) {
+            r.push({ e: e.toString() });
         }
     });
-    for (let i=0, j=1; j<r.length; j++) {
+    for (let i = 0, j = 1; j < r.length; j++) {
         if (r[i].url == r[j].url) {
             r[i].endDate = r[j].date;
             r[j].dup = true;
@@ -765,26 +801,26 @@ let ticketsource = async (tsid) => {
 (handlers["dyfed"] = async () => {
     let r = [];
     try {
-    const json = await ftext("https://dyfedchoir.co.uk/events.json");
-    const strip = json.replace(/\/\*.*?\*\//gs, "").replace(/\s+/," ");
-    const eventList = JSON.parse(strip);
-    if (eventList && eventList.length>0) {
-        r = eventList.map(event => {
-            let dt = new Date(event['date-utc']);
-            return {
-                title: event.title,
-                image: event.image,
-                url: event.book,
-                venue: event.venue,
-                subtitle: event.info,
-                text: event.description,
-                dt: dt.valueOf(),
-                date: dt.toLocaleDateString("en-GB", DMhmformat),
-                category: "live"
-            }
-        });
-    }
-    } catch (e){};
+        const json = await ftext("https://dyfedchoir.co.uk/events.json");
+        const strip = json.replace(/\/\*.*?\*\//gs, "").replace(/\s+/, " ");
+        const eventList = JSON.parse(strip);
+        if (eventList && eventList.length > 0) {
+            r = eventList.map(event => {
+                let dt = new Date(event['date-utc']);
+                return {
+                    title: event.title,
+                    image: event.image,
+                    url: event.book,
+                    venue: event.venue,
+                    subtitle: event.info,
+                    text: event.description,
+                    dt: dt.valueOf(),
+                    date: dt.toLocaleDateString("en-GB", DMhmformat),
+                    category: "live"
+                }
+            });
+        }
+    } catch (e) { };
 
     return r;
 }).friendly = "Cor Dyfed";
@@ -810,7 +846,7 @@ let gigio = async (source, defaultVenue = "") => {
                 subtitle: langSplit(event.meta.dtinfo || ""),
                 dt: new Date(event.meta.dtstart).valueOf(),
                 date: dateRange,
-                category: (event.title.indexOf("NTLive")<0 ? "live" : "broadcast")
+                category: (event.title.indexOf("NTLive") < 0 ? "live" : "broadcast")
             }
         })
     } catch (e) { console.log(e.toString()) }
@@ -926,7 +962,7 @@ module.exports = async function (context, req) {
         } else {
             let kk = Object.keys(handlers);
             r = {};
-            kk.forEach(k => { if (k.indexOf("_")!=0) {r[k] = handlers[k].friendly || k; }});
+            kk.forEach(k => { if (k.indexOf("_") != 0) { r[k] = handlers[k].friendly || k; } });
         }
         context.res = {
             body: JSON.stringify(r),
